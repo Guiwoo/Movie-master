f226801544a689fab71c16fe9dbddd6f1dacb30c
Guiwoo <park.guiwoo@hotmail.com>
2022-01-16 14:11:53 +0900

    update state by javascript way

--- src/components/ListTodo.tsx
+++ src/components/ListTodo.tsx
@@ -3,7 +3,18 @@ import { ItoDoState, toDoAtom } from "../atom";
 
 const ListTodo = ({ text, category, id }: ItoDoState) => {
   const setToDos = useSetRecoilState(toDoAtom);
-  const onClick = (newCat: ItoDoState["category"]) => {};
+  const onClick = (newCat: ItoDoState["category"]) => {
+    console.log(newCat);
+    setToDos((oldTodos) => {
+      const targetIndex = oldTodos.findIndex((e) => e.id === id);
+      const updateTodo = { id, text, category: newCat };
+      return [
+        ...oldTodos.slice(0, targetIndex),
+        updateTodo,
+        ...oldTodos.slice(targetIndex + 1),
+      ];
+    });
+  };
   /**
    * const onClick = (e:React.MouseEvent<HTMLButtonElement>) => {
    *    const {currentTarget:{name}}=e
