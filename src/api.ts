a4d2cb8ce3ca9bcb35c9e6a6cbd48ac3992ed4ab
Guiwoo <park.guiwoo@hotmail.com>
2022-01-23 22:07:31 +0900

    backdrop path setting and define a interface for data

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = "2384348b5a6b3811901d3b50c7882207";
 const BASE_PATH ="https://api.themoviedb.org/3"
 
+interface IMovie {
+backdrop_path: string,
+id: number,
+overview: string,
+poster_path: string,
+title: string,
+}
+
+export interface IGetMovieResult {
+    dates: {
+        maximum: string,
+        minimum: string
+        }
+    page: number,
+    results:IMovie[],
+    total_pages: number,
+    total_results: number,
+}
+
 export const getMovies = () => {
     return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=kr`).then(
         (response)=>response.json()
